b83d08f83d15ead28663d4d18ab970e5855d7f0f
cheng <chengtang827@gmail.com>
2017-09-15 10:36:12 +0800

    add nms_shift function

---
+++ @psthtemp/psthtemp.m
@@ -0,0 +1,82 @@
+function [obj, varargout] = psthtemp(varargin)
+
+
+Args = struct('RedoLevels',0, 'SaveLevels',0, 'Auto',1, 'ArgsOnly',0,'stimulus','target','binLen',50,...
+    'pre',-300,'post',2600);
+Args.flags = {'Auto','ArgsOnly'};
+% The arguments which can be neglected during arguments checking
+Args.DataCheckArgs = {}; 
+
+[Args,modvarargin] = getOptArgs(varargin,Args, ...
+    'subtract',{'RedoLevels','SaveLevels'}, ...
+    'shortcuts',{'redo',{'RedoLevels',1}; 'save',{'SaveLevels',1}}, ...
+    'remove',{'Auto'});
+
+% variable specific to this class. Store in Args so they can be easily
+% passed to createObject and createEmptyObject
+Args.classname = 'psthtemp';
+Args.matname = [Args.classname '.mat'];
+Args.matvarname = 'pst';
+
+% To decide the method to create or load the object
+command = checkObjCreate('ArgsC',Args,'narginC',nargin,'firstVarargin',varargin);
+  
+if(strcmp(command,'createEmptyObjArgs'))
+    varargout{1} = {'Args',Args};
+    obj = createEmptyObject(Args);
+elseif(strcmp(command,'createEmptyObj'))
+    obj = createEmptyObject(Args);
+elseif(strcmp(command,'passedObj'))
+    obj = varargin{1};
+elseif(strcmp(command,'loadObj'))
+    l = load(Args.matname);
+    obj = eval(['l.' Args.matvarname]);
+elseif(strcmp(command,'createObj'))
+    % IMPORTANT NOTICE!!!
+    % If there is additional requirements for creating the object, add
+    % whatever needed here
+    obj = createObject(Args,varargin{:});
+end
+
+function obj = createObject(Args,varargin)
+
+load('dataset_overlapbins_fefdl.mat');
+
+[shift_neurons, resp] = nms_shift(dataset_dl, trials, session_dl, bins_overlap);
+
+
+
+% this is a valid object
+% these are fields that are useful for most objects
+data.numSets = length(shift_neurons);
+data.Args = Args;
+data.shift_neurons = shift_neurons;
+data.resp = resp;
+
+
+% create nptdata so we can inherit from it
+
+data.Args = Args;
+n = nptdata(data.numSets,0,pwd);
+d.data = data;
+obj = class(d,Args.classname,n);
+
+saveObject(obj,'ArgsC',Args);
+
+
+function obj = createEmptyObject(Args)
+
+% useful fields for most objects
+data.numSets = 0;
+data.setNames = '';
+
+% these are object specific fields
+data.dlist = [];
+data.setIndex = [];
+
+% create nptdata so we can inherit from it
+data.Args = Args;
+n = nptdata(0,0);
+d.data = data;
+obj = class(d,Args.classname,n);
+
---
+++ nms_shift.m
@@ -0,0 +1,25 @@
+function [shift_neurons, resp] = nms_shift( dataset,trials,session,bins )
+[nms,~,~,d1_sel,d2_sel,~,~,~,~,~] = TwoWayAnova(dataset,trials,session,bins);
+
+% neurons shifted selectivity are those nms
+% selective both in dl_sel and d2_sel
+temp = d1_sel(ismember(d1_sel,d2_sel));
+shift_neurons = nms(ismember(nms, temp));
+
+
+%resp of the shifted neurons (neuron x location x bin)
+resp = zeros(length(shift_neurons),8,60);
+for i = 1:length(shift_neurons)
+    index = shift_neurons(i);
+    tr1 = session(index,1);
+    n_trials = length(trials(tr1).val);
+    tmp = squeeze(dataset(i,1:n_trials,:));
+    tar = AssignTrialLabel(trials(tr1).val,1);
+    for j = 1:length(unique(tar))
+        ind_tar = tar==j;
+        resp(i,j,:) = mean(tmp(ind_tar,:),1);
+    end
+end
+
+end
+
